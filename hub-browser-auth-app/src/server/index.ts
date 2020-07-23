f928c64d2417a6489a8ff3f62854623b42f21f7f
vasa <vasa@signy.io>
2020-07-23 12:41:52 +0530

    refactor: remove unnecessary dependencies
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- hub-browser-auth-app/src/server/index.ts
+++ hub-browser-auth-app/src/server/index.ts
@@ -2,12 +2,7 @@
 (global as any).WebSocket = require("isomorphic-ws");
 
 import koa from "koa";
-import Router from "koa-router";
-import logger from "koa-logger";
-import json from "koa-json";
-import bodyParser from "koa-bodyparser";
 import websockify from "koa-websocket";
-import cors from "@koa/cors";
 
 import dotenv from "dotenv";
 
@@ -22,20 +17,6 @@ if (!process.env.USER_API_KEY || !process.env.USER_API_SECRET) {
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 
 const app = websockify(new koa());
-
-/** Middlewares */
-app.use(json());
-app.use(logger());
-app.use(bodyParser());
-
-/* Not safe in production */
-app.use(cors());
-
-/**
- * Start HTTP Routes
- */
-const router = new Router();
-app.use(router.routes()).use(router.allowedMethods());
 
 /**
  * Create Websocket endpoint for client-side token challenge
